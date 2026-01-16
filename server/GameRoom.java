fee91e539a898c29a22d6c14bb4b3bdaece3b9c7
LeGiaLam-gif <lamlg0811@ut.edu.vn>
2026-01-16 07:55:41 +0700

    update server

--- server/GameRoom.java
+++ server/GameRoom.java
@@ -1,42 +1,109 @@
 package server;
 
+import common.*;
+
 public class GameRoom {
 
-    private ClientHandler player1;
-    private ClientHandler player2;
+    private final ClientHandler p1;
+    private final ClientHandler p2;
 
-    private String choice1;
-    private String choice2;
+    private String c1, c2;
+    private int replayCount = 0;
 
     public GameRoom(ClientHandler p1, ClientHandler p2) {
-        this.player1 = p1;
-        this.player2 = p2;
+        this.p1 = p1;
+        this.p2 = p2;
+
+        p1.setRoom(this);
+        p2.setRoom(this);
+
+        startGame();
     }
 
-    public synchronized void setChoice(ClientHandler player, String choice) {
-        if (player == player1) {
-            choice1 = choice;
-        } else if (player == player2) {
-            choice2 = choice;
-        }
+    private synchronized void startGame() {
+        c1 = c2 = null;
+        replayCount = 0;
 
-        if (choice1 != null && choice2 != null) {
-            processResult();
+        p1.setPlayerState(PlayerState.PLAYING);
+        p2.setPlayerState(PlayerState.PLAYING);
+
+        sendBoth("Game started");
+        sendBoth("Choose ROCK / PAPER / SCISSORS");
+    }
+
+    private void sendBoth(String msg) {
+        p1.send(new Message(MessageType.INFO, msg));
+        p2.send(new Message(MessageType.INFO, msg));
+    }
+
+    public synchronized void submitChoice(ClientHandler p, String choice) {
+        if (p == p1 && c1 == null)
+            c1 = choice;
+        if (p == p2 && c2 == null)
+            c2 = choice;
+
+        if (c1 != null && c2 != null)
+            evaluate();
+    }
+
+    private void evaluate() {
+        int r = compare(c1, c2);
+
+        if (r == 0)
+            sendResult("DRAW", "DRAW");
+        else if (r == 1)
+            sendResult("WIN", "LOSE");
+        else
+            sendResult("LOSE", "WIN");
+
+        p1.setPlayerState(PlayerState.FINISHED);
+        p2.setPlayerState(PlayerState.FINISHED);
+
+        sendBoth("PLAY_AGAIN or LEAVE or EXIT");
+    }
+
+    private void sendResult(String r1, String r2) {
+        p1.send(new Message(MessageType.RESULT, r1));
+        p2.send(new Message(MessageType.RESULT, r2));
+    }
+
+    public synchronized void requestReplay(ClientHandler p) {
+        replayCount++;
+        if (replayCount == 2)
+            startGame();
+    }
+
+    private int compare(String a, String b) {
+        if (a.equals(b))
+            return 0;
+        if (a.equals("ROCK") && b.equals("SCISSORS"))
+            return 1;
+        if (a.equals("SCISSORS") && b.equals("PAPER"))
+            return 1;
+        if (a.equals("PAPER") && b.equals("ROCK"))
+            return 1;
+        return 2;
+    }
+
+    public void playerDisconnected(ClientHandler p) {
+        ClientHandler other = (p == p1) ? p2 : p1;
+        if (other != null) {
+            other.send(new Message(
+                MessageType.INFO,
+                "Opponent disconnected"
+            ));
+            other.setPlayerState(PlayerState.FINISHED);
         }
     }
 
-    private void processResult() {
-        int result = GameUtils.compare(choice1, choice2);
-
-        if (result == 0) {
-            player1.send("RESULT:HÒA");
-            player2.send("RESULT:HÒA");
-        } else if (result == 1) {
-            player1.send("RESULT:THẮNG");
-            player2.send("RESULT:THUA");
-        } else {
-            player1.send("RESULT:THUA");
-            player2.send("RESULT:THẮNG");
-        }
-    }
 }
+            
+            
+            
+            
+            
+            
+            
+            
+                    
+                    
